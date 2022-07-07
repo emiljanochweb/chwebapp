942f3705fcdb69aeed8253c6562605b75006170d
Endri Bimbli <e.bimbli.esterno@almaviva.it>
2022-07-07 09:52:10 +0200

    refactor wip

--- src/views/LogoContainer.js
+++ src/views/LogoContainer.js
@@ -1,34 +1,36 @@
-import React from "react";
 import { Image, StyleSheet, View } from "react-native";
 import COLORS from "../helpers/colors";
 
-const LogoContainer = ({name}) => {
-   let path = (name === "RegisterForm") ? require('../../assets/registration.jpg') : ((name === "LoginForm") ? require('../../assets/login.jpg') : require('../../assets/logoimage.png'))
+const LogoContainer = ({ name }) => {
+  let path = require("../../assets/logoimage.png");
+  if (name === "RegisterForm") {
+    path = require("../../assets/registration.jpg");
+  }
+  if (name === "LoginForm") {
+    path = require("../../assets/login.jpg");
+  }
 
-   return (
-      <View style={styles.center}>
-         <Image
-            style={styles.image}
-            source={path}
-         />
-      </View>
-   );
+  return (
+    <View style={styles.center}>
+      <Image style={styles.image} source={path} />
+    </View>
+  );
 };
 
 export default LogoContainer;
 
 const styles = StyleSheet.create({
-   center: {
-      alignItems: "center",
-      justifyContent: "center",
-      height: 120,
-      paddingBottom: 10,
-   },
-   image: {
-      flex: 1,
-      resizeMode: "cover",
-      backgroundColor: COLORS.white,
-      flexDirection: "row",
-      width: "100%",
-   },
+  center: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 120,
+    paddingBottom: 10,
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    backgroundColor: COLORS.white,
+    flexDirection: "row",
+    width: "100%",
+  },
 });
