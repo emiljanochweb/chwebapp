ea5c16812c69d4ee46b44ad6e59249d1ab84da82
Endri Bimbli <e.bimbli.esterno@almaviva.it>
2022-07-14 14:24:10 +0200

    flatList implementation

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,33 +1,34 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import COLORS from '../helpers/colors'
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import COLORS from "../helpers/colors";
 
-const Item = ({ title, desc, id}) => {
+const Item = ({ title, desc, id, src }) => {
   return (
     <View style={styles.item} key={id}>
-        <Text style={styles.title}>{title}</Text>
-        <Text style={styles.desc}>{desc}</Text>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.desc}>{desc}</Text>
+      <Image source={src} style={{ width: 100, height: 100 }} />
     </View>
-  )
-}
+  );
+};
 
-export default Item
+export default Item;
 
 const styles = StyleSheet.create({
-    item: {
-        padding: 10,   
-    },
-    title: {
-        width: '100%',
-        fontWeight: 'bold',
-        color: COLORS.darkBlue,
-        fontSize: 20,
-        marginBottom: 5
-    },
-    desc: {
-        width: '100%',
-        fontSize: 16,
-        textAlign: "justify",
-        color: COLORS.black
-    }
-})
+  item: {
+    padding: 10,
+  },
+  title: {
+    width: "100%",
+    fontWeight: "bold",
+    color: COLORS.darkBlue,
+    fontSize: 20,
+    marginBottom: 5,
+  },
+  desc: {
+    width: "100%",
+    fontSize: 16,
+    textAlign: "justify",
+    color: COLORS.black,
+  },
+});
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,58 +1,67 @@
 import { useRoute } from "@react-navigation/native";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { useEffect } from "react";
+import { FlatList, LogBox, StyleSheet, Text, View } from "react-native";
 import Item from "../components/Item";
 import COLORS from "../helpers/colors";
 import { DATA } from "../helpers/constants";
 import LogoContainer from "./LogoContainer";
 
 const Home = () => {
-   const { name } = useRoute();
+  const { name } = useRoute();
 
-   return (
-      <ScrollView contentContainerStyle={styles.container}>
-         <LogoContainer name={name} />
-         <View style={styles.subContainer}>
+  const renderItem = ({ item }) => (
+    <Item title={item.title} desc={item.desc} src={item.logo} />
+  );
+
+  useEffect(() => {
+    LogBox.ignoreLogs(["VirtualizedLists should never be nested"]);
+  }, []);
+
+  return (
+    <FlatList
+      nestedScrollEnabled={true}
+      data={DATA}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
+      ListHeaderComponent={
+        <View style={styles.container}>
+          <LogoContainer name={name} />
+          <View style={styles.subContainer}>
             <Text style={styles.description}>
-               Chweb è un'azienda leader nello sviluppo web, specializzata nello
-               sviluppo di soluzioni software di frontend, backend e mobile. Un
-               team giovane e appassionato di tecnologia che lavora per
-               garantire la miglior User Experience grazie alle competenze di
-               sviluppo di interfacce Web e Mobile. I nostri developer sono
-               professionisti sempre aggiornati sulle ultime tecnologie in grado
-               di sviluppare soluzioni con diversi gradi di complessità. Sono
-               problem solver flessibili e attenti al dettaglio.
+              Chweb è un'azienda leader nello sviluppo web, specializzata nello
+              sviluppo di soluzioni software di frontend, backend e mobile. Un
+              team giovane e appassionato di tecnologia che lavora per garantire
+              la miglior User Experience grazie alle competenze di sviluppo di
+              interfacce Web e Mobile. I nostri developer sono professionisti
+              sempre aggiornati sulle ultime tecnologie in grado di sviluppare
+              soluzioni con diversi gradi di complessità. Sono problem solver
+              flessibili e attenti al dettaglio.
             </Text>
-            {DATA.map((item) => (
-               <Item
-                  key={item.id}
-                  title={item.title}
-                  desc={item.desc}
-                  id={item.id}
-               />
-            ))}
-         </View>
-      </ScrollView>
-   );
+          </View>
+        </View>
+      }
+    />
+  );
 };
 
 export default Home;
 
 const styles = StyleSheet.create({
-   container: {
-      flexGrow: 1,
-      backgroundColor: "#fff",
-   },
-   subContainer: {
-      paddingHorizontal: 20,
-      paddingVertical: 25,
-   },
-   description: {
-      color: COLORS.darkBlue,
-      backgroundColor: COLORS.lightBlue,
-      marginVertical: 20,
-      padding: 10,
-      fontSize: 16,
-      textAlign: "justify",
-      lineHeight: 25,
-   },
+  container: {
+    flexGrow: 1,
+    backgroundColor: "#fff",
+  },
+  subContainer: {
+    paddingHorizontal: 20,
+    paddingVertical: 25,
+  },
+  description: {
+    color: COLORS.darkBlue,
+    backgroundColor: COLORS.lightBlue,
+    marginVertical: 20,
+    padding: 10,
+    fontSize: 16,
+    textAlign: "justify",
+    lineHeight: 25,
+  },
 });
